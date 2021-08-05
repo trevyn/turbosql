ab6742b1560d57ae961d4fdf220c5fe73394330e
trevyn <trevyn-git@protonmail.com>
2021-08-05 16:01:28 +0400

    Create empty `migrations.toml` if it doesn't exist

--- turbosql/build.rs
+++ turbosql/build.rs
@@ -1,7 +1,14 @@
-// this file must exist,
-// so that the OUT_DIR env var is set on build,
-// so that we can find the migrations.toml file,
-// so that we can embed it in your binary,
-// so that we can run your migrations.
+fn main() {
+ let mut path = std::path::PathBuf::new();
+ path.push(std::env::var_os("OUT_DIR").unwrap());
+ path.pop();
+ path.pop();
+ path.pop();
+ path.pop();
+ path.pop();
+ path.push("migrations.toml");
 
-fn main() {}
+ if !path.exists() {
+  std::fs::write(path, "").unwrap();
+ }
+}
