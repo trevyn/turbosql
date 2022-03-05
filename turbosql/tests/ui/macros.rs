5555471f9997006f5027960262a046e35860a104
trevyn <trevyn-git@protonmail.com>
2022-03-05 03:13:48 +0200

    add more ui tests

---
+++ turbosql/tests/ui/macros.rs
@@ -0,0 +1,36 @@
+use turbosql::{execute, select, Turbosql};
+
+#[derive(Turbosql, Default)]
+struct NoOption {
+ rowid: Option<i64>,
+ e: u8,
+}
+
+#[derive(Turbosql, Default)]
+struct NoRowId {
+ age: Option<u8>,
+}
+
+#[derive(Turbosql, Default)]
+struct U64 {
+ rowid: Option<i64>,
+ e: Option<u64>,
+}
+
+#[derive(Turbosql, Default)]
+struct Person {
+ rowid: Option<i64>,
+ name: Option<String>,
+ age: Option<u8>,
+}
+
+fn main() {
+ select!(Person "WHERE age = " 24 " AND name = ?", "Bob").unwrap();
+ select!(Person "WHERE age = " 24 " AND name = $name").unwrap();
+ select!(Person "WHERE age = ?", 1, 2).unwrap();
+ select!("UPDATE person SET age = 1").unwrap();
+ execute!("SELECT 1").unwrap();
+ select!(Person "WHERE nonexistentcolumn = 1").unwrap();
+ select!(Nonexistenttable).unwrap();
+ select!(Vec).unwrap();
+}
