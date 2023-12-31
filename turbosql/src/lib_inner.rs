46da264332ab45ae56dfcc9fd9b300c80cffd10c
trevyn <230691+trevyn@users.noreply.github.com>
2023-12-31 12:15:59 +0400

    Add doc comment for `delete` method

--- turbosql/src/lib_inner.rs
+++ turbosql/src/lib_inner.rs
@@ -34,6 +34,7 @@ pub trait Turbosql {
 	/// Updates this existing row in the database, based on `rowid`, which must be `Some`. All fields are overwritten in the database. On success, returns the number of rows updated, which should be 1.
 	fn update(&self) -> Result<usize, Error>;
 	fn update_batch<T: AsRef<Self>>(rows: &[T]) -> Result<(), Error>;
+	/// Deletes this existing row in the database, based on `rowid`, which must be `Some`. On success, returns the number of rows deleted, which should be 1.
 	fn delete(&self) -> Result<usize, Error>;
 }
 
