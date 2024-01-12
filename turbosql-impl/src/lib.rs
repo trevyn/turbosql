47f2ec25d8c2131b77cff1fac750aca72ecea068
trevyn <230691+trevyn@users.noreply.github.com>
2024-01-12 17:44:17 +0400

    impl Parse for ResultType

--- turbosql-impl/src/lib.rs
+++ turbosql-impl/src/lib.rs
@@ -136,13 +136,36 @@ impl ResultType {
 	}
 }
 
-// impl Parse for ResultType {
-//  fn parse(input: ParseStream) -> Result<Self> {
-//   let path = input.parse::<Path>();
-//   eprintln!("{:#?}", path);
-//   Ok(ResultType {})
-//  }
-// }
+impl Parse for ResultType {
+	fn parse(input: ParseStream) -> Result<Self> {
+		input.parse::<Type>().map(|ty| match ty {
+			Type::Path(TypePath { qself: None, ref path }) => {
+				let path = path.segments.last().unwrap();
+				let mut container = None;
+				let content = match &path.ident {
+					ident if ["Vec", "Option"].contains(&ident.to_string().as_str()) => {
+						container = Some(ident.clone());
+						match path.arguments {
+							PathArguments::AngleBracketed(AngleBracketedGenericArguments { ref args, .. }) => {
+								(args.len() != 1).then(|| abort!(args, "Expected 1 argument, found {}", args.len()));
+								let ty = args.first().unwrap();
+								match ty {
+									GenericArgument::Type(ty) => Content::Type(ty.clone()),
+									_ => abort!(ty, "Expected type, found {:?}", ty),
+								}
+							}
+							_ => abort!(path.arguments, "Expected angle bracketed args, found {:?}", path.arguments),
+						}
+					}
+					_ => Content::Type(ty),
+				};
+				ResultType { container, content }
+			}
+			Type::Array(array) => ResultType { container: None, content: Content::Type(Type::Array(array)) },
+			_ => abort!(ty, "Unknown type {:?}", ty),
+		})
+	}
+}
 
 #[derive(Debug)]
 struct MembersAndCasters {
@@ -394,33 +417,7 @@ fn do_parse_tokens<const T: usize>(input: ParseStream) -> Result<proc_macro2::To
 
 	// Get result type and SQL
 
-	let result_type = input.parse::<Type>().ok().map(|ty| match ty {
-		Type::Path(TypePath { qself: None, ref path }) => {
-			let path = path.segments.last().unwrap();
-			let mut container = None;
-			let content = match &path.ident {
-				ident if ["Vec", "Option"].contains(&ident.to_string().as_str()) => {
-					container = Some(ident.clone());
-					match path.arguments {
-						PathArguments::AngleBracketed(AngleBracketedGenericArguments { ref args, .. }) => {
-							(args.len() != 1).then(|| abort!(args, "Expected 1 argument, found {}", args.len()));
-							let ty = args.first().unwrap();
-							match ty {
-								GenericArgument::Type(ty) => Content::Type(ty.clone()),
-								_ => abort!(ty, "Expected type, found {:?}", ty),
-							}
-						}
-						_ => abort!(path.arguments, "Expected angle bracketed arguments, found {:?}", path.arguments),
-					}
-				}
-				_ => Content::Type(ty),
-			};
-			ResultType { container, content }
-		}
-		Type::Array(array) => ResultType { container: None, content: Content::Type(Type::Array(array)) },
-		_ => abort!(ty, "Unknown type {:?}", ty),
-	});
-
+	let result_type = input.parse::<ResultType>().ok();
 	let (mut sql, params, sql_and_parameters_tokens) = parse_interpolated_sql(input)?;
 
 	// Try validating SQL as-is
